73b14c7899fa505054656dded7086e4ddea366db
ootsirasit <ootsirasit@gmail.com>
2025-01-16 16:38:59 +0700

    [APP-0003] Fix update testcase not response

--- service/update_testcase_submission.go
+++ service/update_testcase_submission.go
@@ -19,12 +19,16 @@ func AddAndUpdateTestCase(channel *amqp.Channel, db_pool *gorm.DB, msg amqp.Deli
 	publisher := redis_client.NewRedisAction(redisClient)
 	err := compileCodeTestcase(db_pool,msgBody)
 	if(err!=nil){
-		channel.Nack(msg.DeliveryTag,false,false)
-		return
+		appErr, ok := err.(*utils.AppError)
+		if ok && (appErr.Name == utils.ERROR_NAME.DATABASE_ERROR || appErr.Name == utils.ERROR_NAME.FUNCTION_ERROR) {
+			channel.Nack(msg.DeliveryTag, false, false)
+			return
+		}
 	}
 	err = publisher.PublishMessage(fmt.Sprintf("testcase-result:%s", msgBody.JobID),"done")
 	if err != nil {
 		fmt.Println("Error publishing to Redis:", err)
+		channel.Nack(msg.DeliveryTag, false, false)
 		return
 	}
 	fmt.Println("FINISHED RUNNING")
@@ -41,6 +45,7 @@ func compileCodeTestcase (db_pool *gorm.DB, msgBody models.ReciveMessage) error{
 	}
 	var labExercise models.LabExercise
 	labExerciseRepo.GetLabExerciseById(&labExercise,exerciseUuid)
+	testcaseEmpty := ""
 	if(len(msgBody.TestCaseList)>0){
 		for i:=0; i<len(msgBody.TestCaseList); i++ {
 			testcaseUuid,err := uuid.Parse(msgBody.TestCaseList[i].TestCaseID)
@@ -66,24 +71,28 @@ func compileCodeTestcase (db_pool *gorm.DB, msgBody models.ReciveMessage) error{
 					}
 					err = exerciseTestcaseRepo.UpdateTestCase(&testcaseData,exerciseUuid)
 					if(err != nil){
-						utils.NewAppError(utils.ERROR_NAME.DATABASE_ERROR,"failed to write file", err.Error())
+						return utils.NewAppError(utils.ERROR_NAME.DATABASE_ERROR,"failed to write file", err.Error())
 					}
 					for j:=i+1; j<len(msgBody.TestCaseList); j++{
 						errorText := "Error occurred before this testcase"
+						testcaseAtJUuid,err := uuid.Parse(msgBody.TestCaseList[j].TestCaseID)
+						if(err!=nil){
+							return utils.NewAppError(utils.ERROR_NAME.FUNCTION_ERROR,"failed to convert testcase uuid", err.Error())
+						}
 						testCaseData := models.ExerciseTestcase{
-							TestcaseID: &testcaseUuid,
+							TestcaseID: &testcaseAtJUuid,
 							ExerciseID: exerciseUuid,
-							TestcaseContent: msgBody.TestCaseList[i].TestCaseContent,
+							TestcaseContent: msgBody.TestCaseList[j].TestCaseContent,
 							IsReady: "no",
-							IsActive: &msgBody.TestCaseList[i].Active,
-							IsShowStudent: &msgBody.TestCaseList[i].ShowToStudent,
-							TestcaseNote: &msgBody.TestCaseList[i].TestCaseNote,
-							TestcaseOutput: nil,
+							IsActive: &msgBody.TestCaseList[j].Active,
+							IsShowStudent: &msgBody.TestCaseList[j].ShowToStudent,
+							TestcaseNote: &msgBody.TestCaseList[j].TestCaseNote,
+							TestcaseOutput: &testcaseEmpty,
 							TestcaseError: &errorText,
 						}
 						err = exerciseTestcaseRepo.UpdateTestCase(&testCaseData,exerciseUuid)
 						if(err != nil){
-							utils.NewAppError(utils.ERROR_NAME.DATABASE_ERROR,"failed to write file", err.Error())
+							return utils.NewAppError(utils.ERROR_NAME.DATABASE_ERROR,"failed to write file", err.Error())
 						}
 					}
 					return utils.NewAppError(appErr.Name,appErr.Error(), appErr.Stdout)
@@ -100,12 +109,12 @@ func compileCodeTestcase (db_pool *gorm.DB, msgBody models.ReciveMessage) error{
 					IsShowStudent: &msgBody.TestCaseList[i].ShowToStudent,
 					TestcaseNote: &msgBody.TestCaseList[i].TestCaseNote,
 					TestcaseOutput: &output,
-					TestcaseError: nil,
+					TestcaseError: &testcaseEmpty,
 				}
 
 				err = exerciseTestcaseRepo.UpdateTestCase(&testCaseData,exerciseUuid)
 				if(err != nil){
-					utils.NewAppError(utils.ERROR_NAME.DATABASE_ERROR,"failed to update testcase to db", err.Error())
+					return utils.NewAppError(utils.ERROR_NAME.DATABASE_ERROR,"failed to update testcase to db", err.Error())
 				}
 				labExerciseRepo.UpdateExerciseTestcaseEnum(&labExercise,"YES")
 			}
